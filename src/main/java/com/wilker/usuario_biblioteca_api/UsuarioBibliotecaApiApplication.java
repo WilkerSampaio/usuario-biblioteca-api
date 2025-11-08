90213d1ecf729bf86206f283ad36057474b18685
Wilker <neoxextreme@gmail.com>
2025-11-07 22:27:31 -0400

    primeiro commit

---
+++ src/main/java/com/wilker/usuario_biblioteca_api/UsuarioBibliotecaApiApplication.java
@@ -0,0 +1,13 @@
+package com.wilker.usuario_biblioteca_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioBibliotecaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioBibliotecaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilker/usuario_biblioteca_api/UsuarioBibliotecaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilker.usuario_biblioteca_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioBibliotecaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
