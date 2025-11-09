f3372fbb20d5aca17d1dc7f2c95c4902740b3d7f
Wilker <neoxextreme@gmail.com>
2025-11-09 11:50:03 -0400

    adicionado GET para buscar usuario pelo email

--- src/main/java/com/wilker/usuario_biblioteca_api/service/UsuarioService.java
+++ src/main/java/com/wilker/usuario_biblioteca_api/service/UsuarioService.java
@@ -5,6 +5,7 @@ import com.wilker.usuario_biblioteca_api.infrastructure.dto.request.UsuarioReque
 import com.wilker.usuario_biblioteca_api.infrastructure.dto.response.UsuarioResponseDTO;
 import com.wilker.usuario_biblioteca_api.infrastructure.entity.UsuarioEntity;
 import com.wilker.usuario_biblioteca_api.infrastructure.exception.ConflictException;
+import com.wilker.usuario_biblioteca_api.infrastructure.exception.ResourceNotFoundException;
 import com.wilker.usuario_biblioteca_api.infrastructure.mapper.UsuarioMapperConverter;
 import com.wilker.usuario_biblioteca_api.infrastructure.repository.UsuarioRepository;
 import com.wilker.usuario_biblioteca_api.infrastructure.security.JwtUtil;
@@ -29,6 +30,7 @@ public class UsuarioService {
     private final JwtUtil jwtUtil;
 
     public UsuarioResponseDTO registraUsuario(UsuarioRequestDTO usuarioRequestDTO){
+
         casoEmailExiste(usuarioRequestDTO.email());
         UsuarioEntity usuarioEntity = new UsuarioEntity();
 
@@ -40,24 +42,36 @@ public class UsuarioService {
     }
 
     public boolean verificaSeExisteEmail (String email){
+
         return usuarioRepository.existsByEmail(email);
     }
+
     public void casoEmailExiste(String email){
         if(verificaSeExisteEmail(email)){
             throw new ConflictException("Email já cadastrado!");
         }
     }
 
-    public String autenticaUsuario(LoginRequestDTO loginRequestDTO){
-        try{
+    public String autenticaUsuario(LoginRequestDTO loginRequestDTO) {
+
+        try {
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginRequestDTO.email(), loginRequestDTO.senha()));
 
-                return "Bearer " + jwtUtil.generateToken(authentication.getName());
-        } catch (BadCredentialsException | UsernameNotFoundException | AuthorizationDeniedException e){
+            return "Bearer " + jwtUtil.generateToken(authentication.getName());
 
+        } catch (BadCredentialsException | UsernameNotFoundException | AuthorizationDeniedException e) {
             throw new UsernameNotFoundException("Credenciais inválidas. Verifique seu email e senha", e.getCause());
         }
+    }
+
+    public UsuarioResponseDTO buscaUsuarioPeloEmail(String email){
+
+        UsuarioEntity usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email não encontrado"));
+
+        return usuarioMapperConverter.paraUsuarioResponseDTO(usuarioEntity);
 
     }
+
 }
