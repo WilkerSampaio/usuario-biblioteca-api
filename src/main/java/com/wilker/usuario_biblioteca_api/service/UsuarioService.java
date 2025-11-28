08def21bcbabae7c018b687db20e3d4df5fb3713
Wilker <neoxextreme@gmail.com>
2025-11-27 23:03:19 -0400

    correção para usuario

--- src/main/java/com/wilker/usuario_biblioteca_api/service/UsuarioService.java
+++ src/main/java/com/wilker/usuario_biblioteca_api/service/UsuarioService.java
@@ -4,6 +4,7 @@ import com.wilker.usuario_biblioteca_api.infrastructure.dto.request.LoginRequest
 import com.wilker.usuario_biblioteca_api.infrastructure.dto.request.UsuarioRequestDTO;
 import com.wilker.usuario_biblioteca_api.infrastructure.dto.response.UsuarioResponseDTO;
 import com.wilker.usuario_biblioteca_api.infrastructure.entity.UsuarioEntity;
+import com.wilker.usuario_biblioteca_api.infrastructure.enums.RoleEnum;
 import com.wilker.usuario_biblioteca_api.infrastructure.exception.ConflictException;
 import com.wilker.usuario_biblioteca_api.infrastructure.exception.ResourceNotFoundException;
 import com.wilker.usuario_biblioteca_api.infrastructure.mapper.UsuarioMapperConverter;
@@ -35,6 +36,7 @@ public class UsuarioService {
         casoEmailExiste(usuarioRequestDTO.email());
 
         UsuarioEntity usuarioEntity = usuarioMapperConverter.paraUsuarioEntity(usuarioRequestDTO);
+        usuarioEntity.setRoles(List.of(RoleEnum.USER));
         usuarioEntity.setSenha(passwordEncoder.encode(usuarioRequestDTO.senha()));
 
         return usuarioMapperConverter.paraUsuarioResponseDTO(usuarioRepository.save(usuarioEntity));
